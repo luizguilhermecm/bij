70a6f5a0be56bd8bcc9bc4cecc17653a5b5a4108
Luiz Guilherme Castilho Martins <luizgui@gmail.com>
2012-08-23 23:09:53 -0300

    cliente estava atribuindo (a) como padra

--- teste/passing-struct/server.c
+++ teste/passing-struct/server.c
@@ -7,6 +7,6 @@ operands * pass_1_svc (operands * argp, struct svc_req *rqstp)
    static operands ops;
    ops.y = argp->y;
    printf ("Recebi chamado: cast %c \n", argp->y);
-   ops.y = 'a';
+   ops.y = argp->y;
    return (&ops);
 }
